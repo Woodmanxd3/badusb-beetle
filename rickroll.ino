23e3f742a58f0f41a091d5067f80dd62b4586504
Woodmanxd3 <101494911+Woodmanxd3@users.noreply.github.com>
2026-07-02 22:32:53 +0000

    Update rickroll.ino

--- rickroll.ino
+++ rickroll.ino
@@ -1,39 +1,46 @@
-#include "Keyboard.h"
+#include <Keyboard.h>
+#include <KeyboardLayout.h>
+#include <Keyboard_da_DK.h>
+#include <Keyboard_de_DE.h>
+#include <Keyboard_es_ES.h>
+#include <Keyboard_fr_FR.h>
+#include <Keyboard_hu_HU.h>
+#include <Keyboard_it_IT.h>
+#include <Keyboard_pt_PT.h>
+#include <Keyboard_sv_SE.h>
 
 void setup() {
-    delay(3000); // Wait for system to be ready
-    Keyboard.begin();
-
-    // Open Run Dialog (Win + R)
-    Keyboard.press(KEY_LEFT_GUI);
-    Keyboard.press('r');
-    Keyboard.releaseAll();
-    delay(500);
-
-    // Open PowerShell
-    Keyboard.print("powershell");
-    Keyboard.press(KEY_RETURN);
-    Keyboard.releaseAll();
-    delay(1500);
-
-    // Command to play video in Microsoft Edge Kiosk Mode
-    Keyboard.print("Start-Process msedge.exe -ArgumentList \"--kiosk https://shattereddisk.github.io/rickroll/rickroll.mp4 --edge-kiosk-type=fullscreen\"");
-    Keyboard.press(KEY_RETURN);
-    Keyboard.releaseAll();
-    delay(1000);
-
-    // If Edge is unavailable, use Windows Media Player
-    Keyboard.print("$wmp = New-Object -ComObject WMPlayer.OCX; $wmp.URL = 'https://shattereddisk.github.io/rickroll/rickroll.mp4'; $wmp.controls.play();");
-    Keyboard.press(KEY_RETURN);
-    Keyboard.releaseAll();
-    delay(1000);
-
-    // Exit PowerShell
-    Keyboard.print("exit");
-    Keyboard.press(KEY_RETURN);
-    Keyboard.releaseAll();
-
-    Keyboard.end();
+  // put your setup code here, to run once:
+delay(3000);
+Keyboard.begin();
+//Press the windows key
+Keyboard.press(KEY_LEFT_GUI);
+Keyboard.releaseAll();
+delay(1000);
+Keyboard.print("n");
+delay(300);
+Keyboard.print("o");
+delay(100);
+Keyboard.print("t");
+delay(100);
+Keyboard.print("e");
+delay(300);
+Keyboard.press(KEY_RETURN);
+Keyboard.releaseAll();
+delay(1000);
+Keyboard.print("Pay");
+delay(50);
+Keyboard.print("load ");
+delay(100);
+Keyboard.print("Succ");
+delay(50);
+Keyboard.print("eeded!");
+//Stop emulating the keyboard
+Keyboard.releaseAll();
+Keyboard.end();
 }
 
-void loop() {}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
